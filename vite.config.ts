86e656eb80e21dbeca22dc43a789f0c7f7b448a5
Gyuchan lim <gyuchanlim@Gyuchans-MacBook-Pro.local>
2025-05-08 15:23:58 +1200

    Allow rendering React using Rails Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
 
+const root = path.resolve(__dirname, "app/javascript");
 export default defineConfig({
   plugins: [
     RubyPlugin(),
   ],
+  resolve: {
+    alias: {
+      "@": root,
+    } as AlaisOptions,
+  },
 })
